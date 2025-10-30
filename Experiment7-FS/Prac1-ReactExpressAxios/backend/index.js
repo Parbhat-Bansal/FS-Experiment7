4b465130353242c19f33133b1c6199468c7fee40
Parbhat-Bansal <154341244+Parbhat-Bansal@users.noreply.github.com>
2025-10-30 20:36:32 +0530

    Add files via upload

---
+++ Experiment7-FS/Prac1-ReactExpressAxios/backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+app.use(cors());
+const products = [
+{ name: "Laptop", price: 1000 },
+{ name: "Phone", price: 500 },
+{ name: "Headphones", price: 100 },
+];
+app.get("/products",(req,res)=
+app.listen(PORT,()= running on http://localhost:${PORT}`));
